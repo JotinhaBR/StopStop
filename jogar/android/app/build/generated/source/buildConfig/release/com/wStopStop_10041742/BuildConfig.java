c0dace0919a742142bcc6e1dfe7c5e222e5cdd9d
Rubens Flinco <planetacraftvip@hotmail.com.br>
2020-05-24 18:52:55 -0300

    corrigindo codigo da versão do apk para 109

--- jogar/android/app/build/generated/source/buildConfig/release/com/wStopStop_10041742/BuildConfig.java
+++ jogar/android/app/build/generated/source/buildConfig/release/com/wStopStop_10041742/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.wStopStop_10041742";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 110;
+  public static final int VERSION_CODE = 109;
   public static final String VERSION_NAME = "1.0.9";
 }
