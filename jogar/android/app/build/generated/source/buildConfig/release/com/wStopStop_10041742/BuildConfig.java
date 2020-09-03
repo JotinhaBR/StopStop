3d9427fcd282efd6f46f838d44d9617c69f4443d
Rubens Flinco <rubens@planetsweb.com.br>
2020-09-03 05:06:28 -0300

    Build android v 1.1.4

--- jogar/android/app/build/generated/source/buildConfig/release/com/wStopStop_10041742/BuildConfig.java
+++ jogar/android/app/build/generated/source/buildConfig/release/com/wStopStop_10041742/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.wStopStop_10041742";
   public static final String BUILD_TYPE = "release";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 112;
-  public static final String VERSION_NAME = "1.1.2";
+  public static final int VERSION_CODE = 114;
+  public static final String VERSION_NAME = "1.1.4";
 }
