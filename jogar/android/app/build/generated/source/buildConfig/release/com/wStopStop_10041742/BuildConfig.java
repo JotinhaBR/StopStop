a55508c68c5c69d240ea73652eb2ebf138913f43
Rubens Flinco <planetacraftvip@hotmail.com.br>
2020-12-14 22:22:15 -0300

    build android nova versão

--- jogar/android/app/build/generated/source/buildConfig/release/com/wStopStop_10041742/BuildConfig.java
+++ jogar/android/app/build/generated/source/buildConfig/release/com/wStopStop_10041742/BuildConfig.java
@@ -7,6 +7,7 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.wStopStop_10041742";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 114;
-  public static final String VERSION_NAME = "1.1.4";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 115;
+  public static final String VERSION_NAME = "1.1.5";
 }
